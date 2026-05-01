0fb1ed2083219a203668b90a5bd1cb26d79c95c1
Tobias Berger <toby@tobot.dev>
2026-05-02 00:24:30 +0200

    Update everything else

--- webpack.config.js
+++ webpack.config.js
@@ -44,7 +44,10 @@ const config = {
 				use: [
 					{
 						loader: "babel-loader",
-						options: { presets: ["@babel/preset-react", "@babel/preset-env"] },
+						options: {
+							presets: ["@babel/preset-react", "@babel/preset-env"],
+							targets: "defaults"
+						},
 					},
 				],
 			},
@@ -54,7 +57,10 @@ const config = {
 				use: [
 					{
 						loader: "babel-loader",
-						options: { presets: ["@babel/preset-env"] },
+						options: {
+							presets: ["@babel/preset-env"],
+							targets: "defaults"
+						},
 					},
 				],
 			},
