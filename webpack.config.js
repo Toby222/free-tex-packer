75e4ac068d0013229842ea896c8b17ec3309a2aa
Tobias Berger <toby@tobot.dev>
2026-05-01 21:58:25 +0200

    minimize output

--- webpack.config.js
+++ webpack.config.js
@@ -7,38 +7,29 @@ const entry = ["babel-polyfill", "./src/client/index"];
 
 const plugins = [];
 
-let output = "static/js/index.js";
-
+const output = argv.build ? "web/static/js/index.js" : "static/js/index.js";
 const NODE_ENV = argv.build ? "production" : "development";
 const devtool = argv.build ? "source-map" : "eval-source-map";
 
-plugins.push(
-	new webpack.DefinePlugin({
-		"process.env.NODE_ENV": JSON.stringify(NODE_ENV),
-	}),
-);
-
-if (argv.build) {
-	plugins.push(
-		new CopyWebpackPlugin([{ from: "src/client/resources", to: "web/" }]),
-	);
-
-	output = "web/static/js/index.js";
-} else {
+if (!argv.build) {
 	entry.push("webpack-dev-server/client?http://localhost:4000");
 	plugins.push(
 		new CopyWebpackPlugin([{ from: "src/client/resources", to: "./" }]),
 	);
+} else {
+	plugins.push(
+		new CopyWebpackPlugin([{ from: "src/client/resources", to: "web/" }]),
+	);
 }
 
 const config = {
-	entry: entry,
+	plugins,
+	entry,
+	devtool,
 	output: {
 		path: __dirname + "/dist",
 		filename: output,
 	},
-	devtool: devtool,
-	target: "web",
 	mode: NODE_ENV,
 	module: {
 		noParse: /.*[/\\]bin[/\\].+\.js/,
@@ -69,10 +60,6 @@ const config = {
 			},
 		],
 	},
-	optimization: {
-		minimize: false,
-	},
-	plugins: plugins,
 };
 
 config.resolve = {
